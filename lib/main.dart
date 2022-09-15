e6ab225a4068722c9a450597a526b211712307a7
Erickricheter <erickricheter@hotmail.com>
2022-09-15 19:16:23 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,6 @@ class MyHomePage extends StatefulWidget {
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
@@ -87,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // "Toggle Debug Paint" action from the Flutter Inspector in Android
           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
           // to see the wireframe for each widget.
-          //
+          // 
           // Column has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
