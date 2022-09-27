bac6e69fc9119b2d357f724f1c4e81553412e097
Indiana Vanilla Dos Santos <indianavanilla@hotmail.com>
2022-09-27 17:37:36 -0300

    HomePageNull
    HomePageNull

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -10,6 +10,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return AppBar(title: Text('Ta indo'));
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('Teste'),
+    ));
   }
 }
